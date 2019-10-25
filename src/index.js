9e40626c4698791ecbc2a5a3cccfcf5082033aab
Gerald Hero Hug <geraldhug92@gmail.com>
2019-10-25 20:43:01 +0800

    'new'

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// named export has a name, have many as you needed
+// default export has no name, you can only have one
+import myLocation, { message, name } from './myFunction';
+
+console.log(name);
+console.log(message);
+console.log(myLocation);
